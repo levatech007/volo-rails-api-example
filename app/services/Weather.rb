3f5bbd2989b30beb0942810b1d395e913f2f4f56
Eva <evaliisagalen@gmail.com>
2018-04-02 19:09:25 -0700

    created config/initializers/my_constants.rb to store constant variables

--- app/services/Weather.rb
+++ app/services/Weather.rb
@@ -6,21 +6,6 @@ class Weather
   end
 
   def get_forecast
-    #wind direction degree range => letter degree
-    wind_directions = {
-      0..11=> 'N', 12..33=> 'NNE', 34..56=> 'NE', 57..78=> 'ENE',
-      79..101=> 'E', 102..123=> 'ESE', 124..146=> 'SE',147..168=> 'SSE',
-      169..191=> 'S',192..213=> 'SSW',214..236=> 'SW',237..258=> 'WSW',
-      259..281=> 'W',282..303=> 'WNW',304..326=> 'NW',327..348=> 'NNW',
-      349..360=> 'N',
-    }
-    days_of_week = {
-      0 => 'Sunday', 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday'
-    }
-    months = {
-      1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June',
-      7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
-    }
     response = RestClient::Request.execute(
       method: :get,
       url: "http://api.openweathermap.org/data/2.5/forecast?lat=#{@lat}&lon=#{@lon}&units=imperial&APPID=#{ ENV['WEATHER_API_KEY'] }"
@@ -34,13 +19,13 @@ class Weather
           day_forecast = {
             'date' => adj_t,
             'day' => adj_t.day,
-            'month' => months.find {|key, val| key === adj_t.month }.last,
-            'day_of_week' => days_of_week.find {|key, val| key === adj_t.wday }.last,
-            'wind_dir' => wind_directions.find {|key, val| key === one_forecast['wind']['deg'].round }.last,
+            'month' => MONTHS.find {|key, val| key === adj_t.month }.last,
+            'day_of_week' => DAYS_OF_WEEK.find {|key, val| key === adj_t.wday }.last,
+            'wind_dir' => WIND_DIRECTIONS.find {|key, val| key === one_forecast['wind']['deg'].round }.last,
             'wind_speed' => one_forecast['wind']['speed'].round,
             'temp' => one_forecast['main']['temp'].round,
             'conditions' => one_forecast['weather'][0]['id'],
-            
+
           }
           @filtered_weather << day_forecast
         end
---
+++ config/initializers/my_constants.rb
@@ -0,0 +1,15 @@
+#constants for external API data processing
+WIND_DIRECTIONS = {
+  0..11=> 'N', 12..33=> 'NNE', 34..56=> 'NE', 57..78=> 'ENE',
+  79..101=> 'E', 102..123=> 'ESE', 124..146=> 'SE',147..168=> 'SSE',
+  169..191=> 'S',192..213=> 'SSW',214..236=> 'SW',237..258=> 'WSW',
+  259..281=> 'W',282..303=> 'WNW',304..326=> 'NW',327..348=> 'NNW',
+  349..360=> 'N',
+}
+DAYS_OF_WEEK = {
+  0 => 'Sunday', 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday'
+}
+MONTHS = {
+  1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June',
+  7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
+}
