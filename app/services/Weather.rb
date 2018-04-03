399eb09deaebe8879cbf1023494eefc1ebf855f9
Eva <evaliisagalen@gmail.com>
2018-04-02 17:49:40 -0700

    added days of the week conversion to Weather class, added rounding to wind speed and temperature

--- app/services/Weather.rb
+++ app/services/Weather.rb
@@ -5,26 +5,16 @@ class Weather
     @lon = lon
   end
 
-
   def get_forecast
     wind_directions = {
-      0..11=> "N",
-      12..33=> "NNE",
-      34..56=> "NE",
-      57..78=> "ENE",
-      79..101=> "E",
-      102..123=> "ESE",
-      124..146=> "SE",
-      147..168=> "SSE",
-      169..191=> "S",
-      192..213=> "SSW",
-      214..236=> "SW",
-      237..258=> "WSW",
-      259..281=> "W",
-      282..303=> "WNW",
-      304..326=> "NW",
-      327..348=> "NNW",
-      349..360=> "N",
+      0..11=> 'N', 12..33=> 'NNE', 34..56=> 'NE', 57..78=> 'ENE',
+      79..101=> 'E', 102..123=> 'ESE', 124..146=> 'SE',147..168=> 'SSE',
+      169..191=> 'S',192..213=> 'SSW',214..236=> 'SW',237..258=> 'WSW',
+      259..281=> 'W',282..303=> 'WNW',304..326=> 'NW',327..348=> 'NNW',
+      349..360=> 'N',
+    }
+    days_of_week = {
+      0 => 'Sunday', 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday'
     }
     response = RestClient::Request.execute(
       method: :get,
@@ -32,16 +22,17 @@ class Weather
       )
       @json_response = JSON.parse(response)
       @filtered_weather = []
-      @json_response['list'].each do |time|
-        t = Time.at(time['dt'])
+      @json_response['list'].each do |one_forecast|
+        t = Time.at(one_forecast['dt'])
         adj_t = t - (60*60*3)
         if adj_t.hour == 14 || adj_t.hour == 2
           day_forecast = {
             'date' => adj_t,
-            'wind_dir' => wind_directions.find {|key, val| key === time['wind']['deg'].round }.last,
-            'wind_speed' => time['wind']['speed'],
-            'temp' => time['main']['temp'],
-            'conditions' => time['weather'][0]['id'],
+            'day_of_week' => days_of_week.find {|key, val| key === adj_t.wday }.last,
+            'wind_dir' => wind_directions.find {|key, val| key === one_forecast['wind']['deg'].round }.last,
+            'wind_speed' => one_forecast['wind']['speed'].round,
+            'temp' => one_forecast['main']['temp'].round,
+            'conditions' => one_forecast['weather'][0]['id'],
           }
           @filtered_weather << day_forecast
         end
