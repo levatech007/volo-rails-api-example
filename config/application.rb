a92baa824e61cf22f883be8ab525bcdd1384b140
Eva <evaliisagalen@gmail.com>
2018-04-24 13:43:29 -0700

    temporarily disabled sidekiq

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module VoloRailsApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config.active_job.queue_adapter = :sidekiq
-    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+    # config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
