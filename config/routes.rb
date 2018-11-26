3d30458b4f88693711979590808d38f64e47843a
Eva <evaliisagalen@gmail.com>
2018-11-26 12:41:35 -0800

    reconfigured/removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,25 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :locations
-  resources :reviews
-  resources :calendars
-  resources :users
+  # root :to => "controller#action"
+  # LOCATIONS
+  get "locations", to: "locations#index"
+  post "locations", to: "locations#create"
+  get "locations/:id", to: "locations#show"
+  patch "locations/:id"  , to: "locations#update"
+  put "locations/:id", to: "locations#update"
+  delete "locations/:id", to: "locations#destroy"
   get "locations/:id/weather", to: "locations#weather", as: "location_weather"
+  # REVIEWS
+  get "reviews", to: "reviews#index"
+  post "reviews", to: "reviews#create"
+  delete "reviews/:id ", to: "reviews#destroy"
+  # CALENDARS
+  get "calendars", to: "calendars#index" # dev only
+  post "calendars", to: "calendars#create"
+  delete "calendars/:id", to: "calendars#destroy"
+
+  resources :users # dev only!
+  # EMAIL LIST
   post "subscribe", to: "email_list#create", as: "subscribe"
 end
