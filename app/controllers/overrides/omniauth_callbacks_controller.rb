a43e0ed7c4d9ae7eab04d50565f09d300cc16f8f
Eva <evaliisagalen@gmail.com>
2019-02-04 20:25:03 -0800

    fixed syntax error

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -5,7 +5,8 @@ module Overrides
       orig_val = ActionController::Parameters.permit_all_parameters
       ActionController::Parameters.permit_all_parameters = true
       permitted_attrs = ActionController::Parameters.new(all_attrs)
-      permitted_attrs.permit({}) user.assign_attributes(permitted_attrs)
+      permitted_attrs.permit({})
+      user.assign_attributes(permitted_attrs)
       ActionController::Parameters.permit_all_parameters = orig_val
       user
     end
