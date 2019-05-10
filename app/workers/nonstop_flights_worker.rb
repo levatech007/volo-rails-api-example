28594aa01d9c194f6c08f82dda60e2fbee84971c
Eva <evaliisagalen@gmail.com>
2019-05-09 20:25:16 -0700

    changed worker perform to include airport code as argument

--- app/workers/nonstop_flights_worker.rb
+++ app/workers/nonstop_flights_worker.rb
@@ -3,12 +3,12 @@ class NonstopFlightsWorker
   sidekiq_options retry: false
   require 'date'
 
-  def perform
+  def perform(airport)
     # check flights for today + 6 days
     dates_to_check_for_flights = (Date.today..Date.today + 6).to_a
     # get all locations
-    uniq_airport_codes = Location.select("DISTINCT airport").map(&:airport)
-    uniq_airport_codes.each do |airport|
+    #uniq_airport_codes = Location.select("DISTINCT airport").map(&:airport)
+    #uniq_airport_codes.each do |airport|
       dates_to_check_for_flights.each do |date|
         formatted_date = date.to_s # use this date format for API call and Redis key
         redis_expiry_unix_date = (date + 8).to_time.to_i # expires in 8 days
@@ -17,16 +17,17 @@ class NonstopFlightsWorker
         p(airport_date_key)
         # check if key already exists in redis
         unless $redis.exists(airport_date_key)
-            # p("No key")
+            p("No key")
             daily_flights = Nonstopflights.new(airport, formatted_date).get_flights
             # p(daily_flights)
             $redis.set(airport_date_key, daily_flights)
             $redis.expireat(airport_date_key, redis_expiry_unix_date)
-            # test = $redis.get(airport_date_key)
-            # p(test)
+            test = $redis.get(airport_date_key)
+            p(test)
+            # WAIT certain amount of time
         end
       end
-    end
+    #end
   end
 
 end
