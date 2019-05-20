7357580318ad1aa3fb1a3f7680b5b9f2283fda17
Eva <evaliisagalen@gmail.com>
2019-05-20 14:00:18 -0700

    added total flights count for fe pagination

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,13 +5,26 @@ class FlightsController < ApplicationController
     aircraft = params[:aircraft] # first 2 digits of aircraft IATA code (two first are always the same for plane type, regardless of variants)
     # retrieve key from Redis
     airport_key = "#{ airport }:nonstop:#{ date }"
-    flights = $redis.get(airport_key)
-    all_flights = JSON.parse(flights)
-    # match the first 2 digits of keys in all_flights
-    # returning matching value
-    flights_by_aircraft = all_flights.select { |key, _| key[0] == aircraft[0] && key[1] == aircraft[1]}
-    # render data
-    render json: { data: flights_by_aircraft }
-    #
+    p(airport_key)
+    # if key is found
+    if $redis.exists(airport_key)
+      flights = $redis.get(airport_key)
+      all_flights = JSON.parse(flights)
+      # match the first 2 digits of keys in all_flights
+      # returning matching value
+      flights_by_aircraft = all_flights.select { |key, _| key[0] == aircraft[0] && key[1] == aircraft[1]}
+      total_flights = flights_by_aircraft.inject(0) { |total, (aircraft,flights)| total + flights.length }
+      p(total_flights)
+      # if flights are found, render data
+      #records_count = 0
+      #flights_by_aircraft.each do |aircraft|
+      unless flights_by_aircraft.empty?
+        render json: { data: flights_by_aircraft, total_flights: total_flights }, status: :ok
+      else
+        render json: { error: "No flights found"}, status: :not_found
+      end
+    else
+      render json: { error: "No flights found"}, status: :not_found
+    end
   end
 end
