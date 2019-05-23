9fca257f09bd6959d3ced58f15c6ebf6164f87f4
Eva <evaliisagalen@gmail.com>
2019-05-22 18:26:46 -0700

    handling for occasions if no flights are found

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,30 +1,37 @@
 class FlightsController < ApplicationController
+
   def get_nonstop_flights
-    airport = params[:airport] # 3 digit IATA airport code
-    date = params[:date] # "YYYY-MM-DD"
+    @airport = params[:airport] # 3 digit IATA airport code
+    @date = params[:date] # "YYYY-MM-DD"
     aircraft = params[:aircraft] # first 2 digits of aircraft IATA code (two first are always the same for plane type, regardless of variants)
-    # retrieve key from Redis
-    airport_key = "#{ airport }:nonstop:#{ date }"
-    p(airport_key)
-    # if key is found
-    if $redis.exists(airport_key)
-      flights = $redis.get(airport_key)
-      all_flights = JSON.parse(flights)
-      # match the first 2 digits of keys in all_flights
-      # returning matching value
-      flights_by_aircraft = all_flights.select { |key, _| key[0] == aircraft[0] && key[1] == aircraft[1]}
-      total_flights = flights_by_aircraft.inject(0) { |total, (aircraft,flights)| total + flights.length }
-      p(total_flights)
-      # if flights are found, render data
-      #records_count = 0
-      #flights_by_aircraft.each do |aircraft|
-      unless flights_by_aircraft.empty?
-        render json: { data: flights_by_aircraft, total_flights: total_flights }, status: :ok
-      else
-        render json: { error: "No flights found"}, status: :not_found
-      end
-    else
-      render json: { error: "No flights found"}, status: :not_found
-    end
+
+    # check if key already exists in redis for airport/date
+    # if yes, retrieve the key
+    # if not, save it to redis
+
+    aviation_api_url = "#{ ENV['AVIATION_API_URL'] }/api/v1/welcome/aircrafts/flights?date=#{ @date }&airport=#{ @airport }&api_key=#{ ENV['AVIATION_API_KEY'] }"
+    response = RestClient::Request.execute(
+      method: :get,
+      url: aviation_api_url,
+      timeout: 90000000
+      ){ |response|
+          p(response)
+
+          if response.code == 200
+
+            all_flights = JSON.parse(response)
+            flights_by_aircraft = all_flights.select { |key, _| key[0] == aircraft[0] && key[1] == aircraft[1]}
+            total_flights = flights_by_aircraft.inject(0) { |total, (aircraft,flights)| total + flights.length }
+            p("Flights by aircraft:")
+            p(flights_by_aircraft)
+            unless flights_by_aircraft.empty?
+              render json: { data: flights_by_aircraft, total_flights: total_flights }, status: :ok
+            else
+              render json: { error: "No flights found"}, status: :not_found
+            end
+          else
+            render json: { error: "No flights found"}, status: :not_found
+          end
+        }
   end
 end
